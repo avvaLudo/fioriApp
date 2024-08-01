588356b36ce8793e693988db5b4b7c244db44380
Ludovica DAmato <ludovica.damato@avvale.com>
2024-08-01 13:27:21 +0000

    changes view on phone, new stable version

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,24 +1,31 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/ui/core/routing/History",
-    "sap/m/MessageToast"
-], (Controller, History, MessageToast) => {
+	"sap/m/MessageToast",
+	"sap/ui/model/json/JSONModel"
+], (Controller, History, MessageToast, JSONModel) => {
 	"use strict";
 
 	return Controller.extend("project1.controller.Detail", {
 		onInit() {
+			const oViewModel = new JSONModel({
+				currency: "EUR"
+			});
+			this.getView().setModel(oViewModel, "view");
+
 			const oRouter = this.getOwnerComponent().getRouter();
 			oRouter.getRoute("detail").attachPatternMatched(this.onObjectMatched, this);
 		},
 
 		onObjectMatched(oEvent) {
-            this.byId("rating").reset();
+			this.byId("rating").reset();
 			this.getView().bindElement({
 				path: "/" + window.decodeURIComponent(oEvent.getParameter("arguments").invoicePath),
 				model: "invoice"
 			});
 		},
-        onNavBack() {
+
+		onNavBack() {
 			const oHistory = History.getInstance();
 			const sPreviousHash = oHistory.getPreviousHash();
 
